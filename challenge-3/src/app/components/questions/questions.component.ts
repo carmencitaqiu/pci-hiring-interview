8635a23be7e139c3df06dcf5164c29edd30be4d9
Eduardo Fernandes <erwor@protonmail.com>
2021-12-03 14:17:40 +0100

    improve explanation

--- challenge-3/src/app/components/questions/questions.component.ts
+++ challenge-3/src/app/components/questions/questions.component.ts
@@ -12,11 +12,11 @@ export class QuestionsComponent implements OnInit {
   ngOnInit(): void {
     /**
      * TODO {2}: Receive the id parameter from the route
-     * Take in count that it will change without reloading
+     * Take in count that it can change without reloading
      * the component.
      * Bonus for validating that the id is a valid number, 
-     * otherwise show error
-     * Double bonus for moving the validation logic to a more
+     * show error if not
+     * Double bonus for moving the route validation logic to a more
      * suitable place (out of this component, that's all i can say :D).
      */
     /**
